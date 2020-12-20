5462bebd69170a3953b818b6a3c61b6f470e023c
Alan Reid <alanreidt@gmail.com>
2020-12-20 19:36:02 +0300

    Apply max-width to Box component (closes #26)

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,14 +1,25 @@
 function Box(props) {
   const {
     padding = '15px 20px',
+    maxWidth,
     children,
   } = props;
 
+  const hasMaxWidth = Number.isFinite(
+    parseFloat(maxWidth)
+  );
+
   const sx = {
+    display: 'block',
+    width: hasMaxWidth ? '100%' : 'auto',
+    maxWidth,
     padding,
+
     backgroundColor: '#eee',
     border: '1px solid black',
     borderRadius: '3px',
+
+    boxSizing: 'border-box',
   };
 
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ function Home() {
 
   return (
     <div style={sx}>
-      <Box>
+      <Box maxWidth={450}>
         <form>
           <Input
             labelText={'Email:'}
