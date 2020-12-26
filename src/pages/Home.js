242e799204b3dfbb9eb878127d34e98758fbac88
Alan Reid <alanreidt@gmail.com>
2020-12-26 19:21:53 +0300

    Add state for Home's inputs (#11)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
+
 import Box from '../components/Box';
 import Input from '../components/Input';
 import Button from '../components/Button';
 
 function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const sx = {
     display: 'flex',
     justifyContent: 'center',
@@ -10,13 +15,24 @@ function Home() {
     minHeight: '100vh',
   };
 
+  const createInputHandler = (setStateAction) => (event) => {
+    const value = event.currentTarget.value;
+
+    setStateAction(value);
+  };
+
+  const handleEmailChange = createInputHandler(setEmail);
+  const handlePasswordChange = createInputHandler(setPassword);
+
   return (
     <div style={sx}>
       <Box maxWidth={450}>
         <form>
           <Input
+            onChange={handleEmailChange}
             labelText={'Email:'}
             type="email"
+            value={email}
             id="email"
             name="email"
             placeholder="georgerowlett@example.com"
@@ -24,15 +40,19 @@ function Home() {
             autoComplete="email"
             required={true}
           />
+
           <Input
+            onChange={handlePasswordChange}
             labelText={'Password:'}
             type="password"
+            value={password}
             id="password"
             name="password"
             placeholder="Enter your password"
             autoComplete="current-password"
             required={true}
           />
+
           <Button>Sign in</Button>
         </form>
       </Box>
