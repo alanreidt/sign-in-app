5d83fbbb9444164e3b4880854a58e64d6b2a0709
Alan Reid <alanreidt@gmail.com>
2020-12-26 20:09:22 +0300

    Add errors handling to the form (closes #11)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,28 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 
 import Box from '../components/Box';
 import Input from '../components/Input';
 import Button from '../components/Button';
 
+const RIGHT_CREDENTIALS = {
+  email: 'joshakins@gmail.com',
+  password: 'pumpkinjuice',
+};
+
+const checkCredentialsRightness = (credentials) => Object.entries(RIGHT_CREDENTIALS).every(
+  ([key, value]) => value === credentials[key]
+);
+
+const Error = styled.p`
+  display: ${(props) => props.show ? 'block' : 'none'};
+  color: red;
+`;
+
 function Home() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [areCredentialsWrong, setAreCredentialsWrong] = useState(false);
 
   const sx = {
     display: 'flex',
@@ -24,10 +40,19 @@ function Home() {
   const handleEmailChange = createInputHandler(setEmail);
   const handlePasswordChange = createInputHandler(setPassword);
 
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    setAreCredentialsWrong(!checkCredentialsRightness({
+      email,
+      password,
+    }));
+  };
+
   return (
     <div style={sx}>
       <Box maxWidth={450}>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <Input
             onChange={handleEmailChange}
             labelText={'Email:'}
@@ -52,6 +77,10 @@ function Home() {
             autoComplete="current-password"
             required={true}
           />
+
+          <Error show={areCredentialsWrong}>
+            {'Wrong email or password'}
+          </Error>
 
           <Button>Sign in</Button>
         </form>
