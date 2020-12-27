776beef7f92e68e3998dfcdf69699098fae364fb
Alan Reid <alanreidt@gmail.com>
2020-12-27 21:24:09 +0300

    Add icons

---
+++ src/icons/VisibilityIcon.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+import { ReactComponent as VisibilityIcon } from '../assets/visibility.svg';
+
+export default styled(VisibilityIcon)`
+  display: inline-block;
+  vertical-align: top;
+`;
---
+++ src/icons/VisibilityOffIcon.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+import { ReactComponent as VisibilityOffIcon } from '../assets/visibility_off.svg';
+
+export default styled(VisibilityOffIcon)`
+  display: inline-block;
+  vertical-align: top;
+`;
