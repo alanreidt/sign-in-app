5e1c404d23e7602b77a0de96679794cce34800d9
Alan Reid <alanreidt@gmail.com>
2020-12-27 20:18:36 +0300

    Replace Input inline styles by styled-components (closes #47)

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,3 +1,27 @@
+import styled from 'styled-components';
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 5px;
+
+  font-size: 18px;
+`;
+
+const StyledInput = styled.input`
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  font-family: inherit;
+  font-size: 16px;
+
+  color: inherit;
+  border: 1px solid rgba(0, 0, 0, .25);
+  border-radius: 3px;
+
+  box-sizing: border-box;
+`;
+
 function Input(props) {
   const {
     labelText,
@@ -5,35 +29,12 @@ function Input(props) {
     ...restProps
   } = props;
 
-  const labelStyle = {
-    display: 'block',
-    marginBottom: '5px',
-
-    fontSize: '18px',
-  };
-
-  const inputStyle = {
-    display: 'block',
-    width: '100%',
-    padding: '15px',
-
-    fontFamily: 'inherit',
-    fontSize: '16px',
-
-    color: 'inherit',
-    border: '1px solid rgba(0, 0, 0, .25)',
-    borderRadius: '3px',
-
-    boxSizing: 'border-box',
-  };
-
   return (
     <div>
-      <label style={labelStyle} htmlFor={id}>
+      <Label htmlFor={id}>
         {labelText}
-      </label>
-      <input
-        style={inputStyle}
+      </Label>
+      <StyledInput
         id={id}
         {...restProps}
       />
