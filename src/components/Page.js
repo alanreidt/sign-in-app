38a0761ce4a7f0eb23969701b41314fbbbaeaa59
Alan Reid <alanreidt@gmail.com>
2021-01-01 17:52:30 +0300

    Add responsive styles (closes #40)

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,8 +3,14 @@ import styled from 'styled-components';
 const StyledPage = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+
   min-height: 100vh;
+  margin-top: 100px;
+
+  @media(max-height: 400px) {
+    margin-top: 10px;
+  }
 `;
 
 function Page(props) {
