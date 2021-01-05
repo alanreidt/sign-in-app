2f73243c2bc1d3ca2689e4f1e88ffb1523284524
Alan Reid <alanreidt@gmail.com>
2021-01-05 10:49:15 +0300

    Add useAsync hook

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,4 +1,9 @@
-import { useState, useContext } from 'react';
+import {
+  useState,
+  useEffect,
+  useCallback,
+  useContext,
+} from 'react';
 
 import fakeAuth from './fakeAuth';
 import { authContext } from './constants';
@@ -31,7 +36,38 @@ function useAuth() {
   return useContext(authContext);
 }
 
+function useAsync(asyncFunction, immediate = true) {
+  const [status, setStatus] = useState('idle');
+  const [value, setValue] = useState(null);
+  const [error, setError] = useState(null);
+
+  const execute = useCallback(() => {
+    setStatus('pending');
+    setValue(null);
+    setError(null);
+
+    return asyncFunction()
+      .then(response => {
+        setValue(response);
+        setStatus('success');
+      })
+      .catch(error => {
+        setError(error);
+        setStatus('error');
+      });
+  }, [asyncFunction]);
+
+  useEffect(() => {
+    if (immediate) {
+      execute();
+    }
+  }, [execute, immediate]);
+
+  return { execute, status, value, error };
+};
+
 export {
   useProvideAuth,
   useAuth,
+  useAsync,
 }
